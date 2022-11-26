0572c96b25a3a9bbc836bd3a3d8c65fd59c4f3e4
Rafael "Lestat" Kuhn <rafa.faelk@gmail.com>
2022-11-26 18:50:14 -0300

    add: cartesian coordinates

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const canvas = document.getElementById("canvao");
 
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
-ctx.lineWidth *= 3;
 
 const canvasRect = canvas.getBoundingClientRect();
 
 const TAU = 6.28318530;
 
 const pointSize = 7;
+// const coordinateSystemMax = 16;
+const coordinateSystemMax = 8;
 
 // Points for the curve
 let start = { x: 54,  y: 260 };
@@ -43,52 +44,27 @@ const getClampedRelativeMousePos = (event) => {
 const onMouseDown = (event) => {
 	const mousePos = getClampedRelativeMousePos(event);
 
-	const dist0 = distanceTo(mousePos, start);
-	if (dist0 < 20) {
-		// console.log("holding obj st");
-		mouseData.isHolding = true;
-		mouseData.objBeingHold = start;
-		return;
-	}
+	mouseData.objBeingHold = getNearbyClosestObjectOrNull(mousePos);
+	mouseData.isHolding = mouseData.objBeingHold != null;
 
-	const dist1 = distanceTo(mousePos, cp1);
-	if (dist1 < 20) {
-		// console.log("holding obj cp1");
-		mouseData.isHolding = true;
-		mouseData.objBeingHold = cp1;
-		return;
-	}
-
-	const dist2 = distanceTo(mousePos, cp2);
-	if (dist2 < 20) {
-		// console.log("holding obj cp2");
-		mouseData.isHolding = true;
-		mouseData.objBeingHold = cp2;
-		return;
-	}
-
-	const dist3 = distanceTo(mousePos, end);
-	if (dist3 < 20) {
-		// console.log("holding obj end");
-		mouseData.isHolding = true;
-		mouseData.objBeingHold = end;
-		return;
-	}
-
-	mouseData.isHolding = false;
+	mouseMove(event);
 }
+
 
 // /** @param {MouseEvent} event */
 const onMouseUp = () => {
 	mouseData.isHolding = false;
-	mouseData.objBeingHold = false;
+	mouseData.objBeingHold = null;
 }
 
 /** @param {MouseEvent} event */
 const mouseMove = (event) => {
-	if (!mouseData.isHolding || mouseData.objBeingHold == null) return;
-
 	const mousePos = getClampedRelativeMousePos(event);
+
+	const objectBeingHovered = getNearbyClosestObjectOrNull(mousePos);
+	document.body.style.cursor = objectBeingHovered == null ? "default" : "pointer";
+
+	if (!mouseData.isHolding)	return;
 
 	mouseData.objBeingHold.x = mousePos.x;
 	mouseData.objBeingHold.y = mousePos.y;
@@ -105,10 +81,48 @@ const distanceTo = (point0, point1) => {
 	return Math.sqrt(pointsVec.x * pointsVec.x + pointsVec.y * pointsVec.y);
 }
 
+/** @param {{ x: Number, y: Number }} mousePos */
+const getNearbyClosestObjectOrNull = (mousePos) => {
+	if (distanceTo(mousePos, start) < 20) return start;
+	if (distanceTo(mousePos, cp1) < 20)   return cp1;
+	if (distanceTo(mousePos, cp2) < 20)   return cp2;
+	if (distanceTo(mousePos, end) < 20)   return end;
+
+	return null;
+}
+
 const drawStuff = () => {
-	
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.setLineDash([0]);
+	ctx.lineWidth = 3;
+
+
+	ctx.lineWidth = 2;
+	ctx.setLineDash([0]);
+	ctx.fillStyle = "black";
+	ctx.font = "24px serif";
+
+	const period = canvas.width / coordinateSystemMax;
+	for (let i = 1; i < coordinateSystemMax; ++i) {
+		const ourPeriod = period * i;
+
+		// horizontal cartesian coordinates
+		ctx.beginPath();
+		ctx.moveTo(ourPeriod, canvas.height);
+		ctx.lineTo(ourPeriod, canvas.height - 10);
+		ctx.stroke();
+		
+		ctx.fillText(i, ourPeriod - 6, canvas.height - 15);
+		
+		// vertical cartesian coordinates
+		ctx.beginPath();
+		ctx.moveTo(0, ourPeriod);
+		ctx.lineTo(10, ourPeriod);
+		ctx.stroke();
+
+		ctx.fillText(i, 15, canvas.height - ourPeriod + 6);
+	}
+
 
 	// cubic Bézier curve
 	ctx.beginPath();
@@ -130,28 +144,30 @@ const drawStuff = () => {
 	ctx.stroke();
 
 	// start and end points
-	ctx.fillStyle = 'blue';
+	ctx.fillStyle = "blue";
 	ctx.beginPath();
 	ctx.arc(start.x, start.y, pointSize, 0, TAU);
 	ctx.arc(end.x,   end.y,   pointSize, 0, TAU);
 	ctx.fill();
 
 	// control points
-	ctx.fillStyle = 'red';
+	ctx.fillStyle = "red";
 	ctx.beginPath();
 	ctx.arc(cp1.x, cp1.y, pointSize, 0, TAU);
 	ctx.arc(cp2.x, cp2.y, pointSize, 0, TAU);
 	ctx.fill();
 
+	// determine if it's valid
 	const maxXVertices = Math.max(start.x, end.x);
 	const minXVertices = Math.min(start.x, end.x);
-
 	const isValid = cp1.x > minXVertices
 	&& cp2.x > minXVertices
 	&& cp1.x < maxXVertices
 	&& cp2.x < maxXVertices;
 
 	canvas.style.backgroundColor = isValid ? "beige" : "magenta";
+
+	
 }
 
 window.addEventListener("mousedown", onMouseDown)
